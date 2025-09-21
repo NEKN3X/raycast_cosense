f0a52902b9f76532008b5075ca60d2a3dcbdff3d
NEKN3X <43030839+NEKN3X@users.noreply.github.com>
2025-09-21 20:30:13 +0900

    search pages

--- src/search-pages.tsx
+++ src/search-pages.tsx
@@ -1,37 +1,68 @@
-import { getPreferenceValues, List } from "@raycast/api";
-import { useEffect, useState, useCallback } from "react";
-import { debounce } from "./utils";
+import { Action, ActionPanel, getPreferenceValues, List } from "@raycast/api";
+import { useState, useRef } from "react";
+import { usePromise } from "@raycast/utils";
 
 interface Preferences {
   sid: string;
   projects: string;
 }
 
+interface SearchResponse {
+  projectName: string;
+  searchQuery: string;
+  pages: Array<{
+    id: string;
+    title: string;
+    image: string;
+    lines: string[];
+  }>;
+}
+
 export default function Command() {
   const [searchText, setSearchText] = useState("");
-  const preferences = getPreferenceValues<Preferences>(); // TODO: 検索API実装時に使用
+  const preferences = getPreferenceValues<Preferences>();
+  const projects = preferences.projects.split(",").map((p) => p.trim());
+  const urlList = projects.map((project) => `https://scrapbox.io/api/pages/${project}/search/query?q=${searchText}`);
 
-  const debouncedSearch = useCallback(
-    debounce((...args) => {
-      const query = args[0] as string;
-    }, 300),
-    [],
+  const abortable = useRef<AbortController>(undefined);
+  const { isLoading, data } = usePromise(
+    async (urlList: string[]) => {
+      if (urlList.length === 0 || searchText.trim() === "") {
+        return [];
+      }
+      const responses = await Promise.all(
+        urlList.map((u) =>
+          fetch(u, { signal: abortable.current?.signal, headers: { Cookie: `connect.sid=${preferences.sid}` } }),
+        ),
+      );
+      const results = await Promise.all(responses.map((r) => r.json()));
+      return results as SearchResponse[];
+    },
+    [urlList],
+    {
+      abortable,
+    },
   );
 
-  useEffect(() => {
-    if (searchText.trim()) {
-      debouncedSearch(searchText);
-    }
-  }, [searchText, debouncedSearch]);
-
   return (
-    <List onSearchTextChange={setSearchText}>
-      <List.Section title="Lager">
-        <List.Item title="Camden Hells" />
-      </List.Section>
-      <List.Section title="IPA">
-        <List.Item title="Sierra Nevada IPA" />
-      </List.Section>
+    <List onSearchTextChange={setSearchText} isLoading={isLoading}>
+      {data?.map((searchResult) => (
+        <List.Section key={searchResult.projectName} title={searchResult.projectName}>
+          {searchResult.pages.map((page) => (
+            <List.Item
+              key={page.id}
+              title={page.title}
+              icon={page.image}
+              subtitle={page.lines[0]}
+              actions={
+                <ActionPanel title="#1 in raycast/extensions">
+                  <Action.OpenInBrowser url={`https://scrapbox.io/${searchResult.projectName}/${page.title}`} />
+                </ActionPanel>
+              }
+            />
+          ))}
+        </List.Section>
+      ))}
     </List>
   );
 }
--- src/utils.ts
+++
@@ -1,8 +0,0 @@
-export function debounce(func: (...args: unknown[]) => void, delay: number) {
-  let timeoutId: NodeJS.Timeout;
-
-  return (...args: unknown[]) => {
-    clearTimeout(timeoutId);
-    timeoutId = setTimeout(() => func(...args), delay);
-  };
-}
